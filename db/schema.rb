2a76ddb0e8236c89a76ff9d3214730665f6452ed
Jon Malis <jmalis@live.com>
2015-09-08 15:56:32 -0700

    created profile page

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20150907182441) do
 
   create_table "models", force: :cascade do |t|
+    
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -37,6 +38,7 @@ ActiveRecord::Schema.define(version: 20150907182441) do
   end
 
   create_table "users", force: :cascade do |t|
+
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
